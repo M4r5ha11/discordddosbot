28d4741dd909426c5ddff9b50fb60001e9b753d1
M4r5ha11 <68496774+M4r5ha11@users.noreply.github.com>
2020-10-29 09:55:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const fetch = require("node-fetch");
+
+client.on("message", message => {
+  if (message.content === "!") {
+    message.reply("it already work");
+    setInterval(() => fetch("http://site.com"), 1);
+  }
+});
+
+//client.login('yourtoken');
